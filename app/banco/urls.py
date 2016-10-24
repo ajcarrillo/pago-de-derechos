d1580bfb227f7751adf91c5344ab1630fdaf96ae
Andrés Carrillo <andresjch2804@gmail.com>
2016-10-24 13:50:29 -0500

    add bancos api endpoint

---
+++ app/banco/urls.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*
+from django.conf.urls import url
+
+from app.banco import views
+
+# **************************************
+#
+#   url: /bancos/
+#   namespace: bancos
+#
+# **************************************
+
+urlpatterns = [
+    url(r'^$', views.BancoListView.as_view(), name='list'),
+    url(r'^(?P<pk>[\d]+)$', views.BancoListView.as_view(), name='list')
+]
--- app/banco/views.py
+++ app/banco/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from app.general.mixins import JsonResponseMixin
+from app.solicitud_pago.models import Banco
+
+
+class BancoListView(JsonResponseMixin, generic.ListView):
+    model = Banco
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        return self.response_handler()
+
+    def get_data(self):
+        data = [{
+                    'pk':          banco.pk,
+                    'referencia':  banco.referencia,
+                    'descripcion': banco.descripcion,
+                } for banco in self.object_list]
+
+        return data
+
+    def get_queryset(self):
+        if self.kwargs.get('name'):
+            queryset = self.model.objects.filter(referencia__contains=self.kwargs['name'])
+        elif self.kwargs.get('pk'):
+            queryset = self.model.objects.get(pk=self.kwargs['pk'])
+        else:
+            queryset = super(BancoListView, self).get_queryset()
+
+        return queryset
