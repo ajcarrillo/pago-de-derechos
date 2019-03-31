5b2981a14400e5d3ef3c4ffd904e9750e544c2d0
Andres Carrillo <andresjch2804@gmail.com>
2019-03-31 18:35:09 -0500

    Crea endpoint para verficar si es posible conectar con el sistema de pago

--- app/general/urls.py
+++ app/general/urls.py
@@ -1,8 +1,9 @@
 # -*- encoding: utf-8 -*
 from django.conf.urls import url
 
-from app.general.views import RootRedirectView
+from app.general.views import RootRedirectView, check_service_on_line
 
 urlpatterns = [
     url(r'^$', RootRedirectView.as_view(), name='redirect_to_admin'),
+    url(r'^check-service-on-line/$', check_service_on_line),
 ]
--- app/general/views.py
+++ app/general/views.py
@@ -1,5 +1,11 @@
+from django.http import JsonResponse
 from django.views import generic
 
 
 class RootRedirectView(generic.RedirectView):
     url = '/admin'
+
+
+def check_service_on_line(request):
+    response = JsonResponse({'foo': 'bar'})
+    return response
